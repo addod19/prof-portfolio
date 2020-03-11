d145db78010087f0ea13f9d7fa3aea92795671c5
Daniel <addodaniellarbi@gmail.com>
2020-03-11 11:51:28 +0000

    added Developer module

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ const Project = (title, description, liveLink, sourceLink, image, ...skills) => 
     return {getTitle, getDescription, getSkills, getLiveLink, getSourceLink, getImage};
 };
 
-let p = Project('javascript', 'THis project was build with vanilla javascript', 'https://www.facebook.com', 'thhps:github.com/addod19','fist.png');
+const Developer = (intro, currPos, ...socNetworks) => {
+    const getIntro = () => intro;
+    const getCurrPos = () => currPos;
+    const getSocNetworks = () => socNetworks;
 
+    return {
+        getIntro,
+        getCurrPos,
+        getSocNetworks
+    }
+};
+
+let dan = Developer('Hi, I am Daniel Larbi Addo a full-stack developer.',
+                    'I am currently working as a technical support engineer (PT)',
+                    ['link1', 'link2', 'link3']);
+dan;
+let p = Project('javascript', 'THis project was build with vanilla javascript', 'https://www.facebook.com', 'thhps:github.com/addod19','fist.png');
+p;
 alert('hi')
--- src/project.js
+++ src/project.js
@@ -1,12 +1,12 @@
-const Project = (title, description, liveLink, sourceLink, image, ...skills) => {
-    const getTitle = () => title;
-    const getDescription = () => description;
-    const getSkills = () => skills;
-    const getLiveLink = () => liveLink;
-    const getSourceLink = () => sourceLink;
-    const getImage = () => image;
+// const Project = (title, description, liveLink, sourceLink, image, ...skills) => {
+//     const getTitle = () => title;
+//     const getDescription = () => description;
+//     const getSkills = () => skills;
+//     const getLiveLink = () => liveLink;
+//     const getSourceLink = () => sourceLink;
+//     const getImage = () => image;
 
-    return {getTitle, getDescription, getSkills, getLiveLink, getSourceLink, getImage};
-};
+//     return {getTitle, getDescription, getSkills, getLiveLink, getSourceLink, getImage};
+// };
 
-export default Project;
+// export default Project;
